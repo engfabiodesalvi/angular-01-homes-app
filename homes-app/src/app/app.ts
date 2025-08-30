d9367c4cdf546a60b709ebfa8493141d9d62699d
engfabiodesalvi <engfabiodesalvi@gmail.com>
2025-08-30 17:40:12 -0300

    First steps. Initial configs

---
+++ homes-app/src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Home],
+  templateUrl: './app.html',
+  // template: `
+  //   <main>
+  //     <header class="brand-name">
+  //       <img class="brand-logo" src="/asserts/logo.svg" alt="logo" aria-hidden="true" />
+  //     </header>
+  //     <section class="content">
+  //       <app-home></app-home>
+  //     </section>
+  //   </main>
+  // `,  
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('homes-app');
+}
